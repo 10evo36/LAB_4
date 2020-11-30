308dec62a3cba0643cb7ab60e4ee02101e00f411
10evo36 <75244122+10evo36@users.noreply.github.com>
2020-12-01 02:09:21 +0300

    Add files via upload

---
+++ individual1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+if __name__ == '__main__':
+    Named = str(input(" Введите название футбольной команды "))
+    s = int(len(Named))
+    for i in range(s):
+        print(Named[i])
